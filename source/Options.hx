504b20177dbd48896eab80a0908dcd0cf3c96bc9
Just a guy <103865615+just-a-guy1532@users.noreply.github.com>
2022-09-04 16:12:01 +0100

    Update Options.hx

--- source/Options.hx
+++ source/Options.hx
@@ -98,6 +98,43 @@ class DFJKOption extends Option
 	}
 }
 
+class CustomControls extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.switchState(new options.CustomControlsState());
+		return true;
+	}
+	private override function updateDisplay():String
+	{
+		return "controls";
+	}
+
+}
+class About extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.switchState(new options.AboutState());
+		return true;
+	}
+	private override function updateDisplay():String
+	{
+		return "About";
+	}
+
+}
+
 class DownscrollOption extends Option
 {
 	public function new(desc:String)
